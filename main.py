cb6076d2f456fa86ac9c2261086fc2bee32a019f
Bryan Luu <bryanluu30794@gmail.com>
2017-03-06 22:31:09 -0800

    Added a robust space demo of a planet orbiting a sun.

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import pygame
+from roche import Environment, Planet
+from geometry import Vector2D
 
-class Environment:
-    """ Defines the boundary of a simulation and its properties """
 
-    def __init__(self, (width, height)):
-        self.width = width
-        self.height = height
-        self.colour = (255, 255, 255)
+# =========== START OF SIMULATION CODE ============
 
 (width, height) = (800, 800)
 screen = pygame.display.set_mode((width, height))
@@ -15,11 +12,32 @@ pygame.display.set_caption('Gravity Test')
 universe = Environment((width, height))
 universe.colour = (0,0,0)
 
+sun_radius = 50
+sun = Planet((width/2, height/2), sun_radius, density=1)
+sun.fixed = True
+sun.colour = (255, 255, 0)
+universe.planets.append(sun)
+
+planet = Planet((100, 400), 15, density=0.5)
+planet.velocity = Vector2D(0, 5)
+planet.colour = (100, 100, 255)
+universe.planets.append(planet)
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    universe.update()
     screen.fill(universe.colour)
+
+    for p in universe.planets:
+        p.move()
+        # Draws it so that (0,0) is the bottom left corner
+        if p.size < 2:
+            pygame.draw.rect(screen, p.colour, (int(p.position.x), height - int(p.position.y), 2, 2))
+        else:
+            pygame.draw.circle(screen, p.colour, (int(p.position.x), height - int(p.position.y)), int(p.size), 0)
+
     pygame.display.flip()
---
+++ roche.py
@@ -0,0 +1,80 @@
+import math
+import random
+from geometry import Vector2D
+
+# Gravitational Constant
+G = 0.2
+
+class Environment:
+    """ Defines the boundary of a simulation and its properties """
+
+    def __init__(self, (width, height)):
+        self.width = width
+        self.height = height
+        self.colour = (255, 255, 255)
+        self.planets = []
+
+    def addPlanets(self, n=1, **kargs):
+        """ Add n planets with properties given by keyword arguments """
+
+        for i in range(n):
+            size = kargs.get('size', random.randint(10, 20))
+            if 'density' in kargs:
+                density = kargs.get('density', 1)
+                mass = density * (size ** 2)
+            else:
+                mass = kargs.get('mass', random.randint(100, 10000))
+
+            x = kargs.get('x', random.uniform(size, self.width - size))
+            y = kargs.get('y', random.uniform(size, self.height - size))
+
+            planet = Planet((x, y), size, mass)
+            speed = kargs.get('speed', random.random())
+            angle = kargs.get('angle', random.uniform(0, math.pi * 2))
+            planet.velocity = Vector2D.create_from_angle(angle, speed)
+            planet.colour = kargs.get('colour', (0, 0, 255))
+
+            self.planets.append(planet)
+
+    def update(self):
+        """  Calls particle functions """
+
+        for i, planet in enumerate(self.planets):
+            # One planet functions get called here
+            planet.move()
+
+            # Two planet functions get called here
+            for planet2 in self.planets[i + 1:]:
+                planet.attract(planet2)
+
+class Planet:
+    """ A circular planet with a velocity, size and density """
+
+    def __init__(self, (x, y), size, density=1):
+        self.position = Vector2D(x, y)
+        self.size = size
+        self.colour = (255, 255, 255)
+        self.thickness = 0
+        self.density = density
+        # cube the radius for a sphere
+        self.mass = density * size ** 3
+        self.velocity = Vector2D.zero()
+        self.fixed = False
+
+    def move(self):
+        """ Update position based on velocity """
+        if not self.fixed:
+            self.position += self.velocity
+
+    def accelerate(self, acceleration):
+        """ Change velocity by vector a """
+        self.velocity += acceleration
+
+    def attract(self, other):
+        dr = self.position - other.position
+        dist = dr.length()
+
+        theta = dr.angle()
+        force = G * self.mass * other.mass / dist ** 2
+        self.accelerate(-Vector2D.create_from_angle(theta, force / self.mass))
+        other.accelerate(Vector2D.create_from_angle(theta, force / other.mass))
