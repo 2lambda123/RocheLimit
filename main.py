2c1f3d4e18f0dbf2315ee244e1fa30477e0a0d48
Bryan Luu <bryanluu30794@gmail.com>
2017-03-13 11:57:02 -0700

    Added an escape key check to close the program.

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
     universe.update(dt)
     screen.fill(universe.colour)
